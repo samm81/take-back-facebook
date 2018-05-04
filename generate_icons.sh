d5ef4013f069bc4d4dd3a88087acbe42be75561d
samm81 <samwmaynard@gmail.com>
2018-05-04 00:45:34 -0500

    shitty new compliant icons

---
+++ generate_icons.sh
@@ -0,0 +1,4 @@
+convert -background gray -fill white -font Helvetica-Bold -size 128x128 -gravity center label:'TBF' icon-128.png
+convert -background gray -fill white -font Helvetica-Bold -size 48x48 -gravity center label:'TBF' icon-48.png
+convert -background gray -fill white -font Helvetica-Bold -size 32x32 -gravity center label:'TBF' icon-32.png
+convert -background gray -fill white -font Helvetica-Bold -size 16x16 -gravity center label:'TBF' icon-16.png
--- generate_promos.sh
+++ generate_promos.sh
@@ -1,4 +1,3 @@
 convert -background gray -fill white -font Helvetica-Bold -size 440x280 -gravity center -pointsize 68 label:'Take Back\nFacebook' promo440x280.png
 convert -background gray -fill white -font Helvetica-Bold -size 920x680 -gravity center -pointsize 88 label:'Take Back\nFacebook' promo920x680.png
 convert -background gray -fill white -font Helvetica-Bold -size 1400x560 -gravity center -pointsize 68 label:'Take Back Facebook' promo1400x560.png
-
