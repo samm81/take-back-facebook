fffa65fea3551d9dbeccc03ff1e01f8c1b2132e5
samm81 <samwmaynard@gmail.com>
2018-07-22 21:04:13 -0700

    updates bundle script to properly work, bumps version

--- bundle.sh
+++ bundle.sh
@@ -2,4 +2,4 @@ set -e
 set -x
 
 rm tbf.zip
-zip tbf.zip manifest.json tbf.js tbf.css fb_gray.ico icon-128.png icon-48.png icon-16.png
+zip tbf.zip background.js fb_gray.ico graybadges.css gray.css icon-128.png icon-16.png icon-32.png icon-48.png likebutton.css likes.css manifest.json options.css options.html options.js popups.css remove.css tbf.css tbf.js
