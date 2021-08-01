9569542403defb99b683a8111eaf45e2a1f1b21e
samm81 <samwmaynard@gmail.com>
2021-08-01 00:39:40 -0700

    2019-06-28

--- background.js
+++ background.js
@@ -1,5 +1,24 @@
 'use strict';
 
+/*
+// https://stackoverflow.com/questions/15765451/chrome-extension-to-efficiently-block-domains
+chrome.webRequest.onBeforeRequest.addListener(
+	function(details) {
+		console.log('caught request');
+		console.log(details);
+		var dyn = details.url.match(/__dyn=(.*?)&/)[1];
+		console.log(dyn);
+		console.log(dyn.length);
+		return { cancel: (dyn.length < 250) };
+	},
+	{ urls: [ "*://*.facebook.com/ajax/pagelet/generic.php/LitestandTailLoadPagelet*" ] },
+	[ "blocking" ]
+);
+*/
+
+// if I try this again, need to add to manifest.js:
+// "permissions": [ "webRequest", "webRequestBlocking", "tabs", "*://*.facebook.com/*" ],
+
 chrome.runtime.onInstalled.addListener(function (details) {
   const defaultoptions = { 'tbf_init': true, 'gray': true, 'graybadges': true, 'freeze': true, 'likes': false, 'likebutton': false, 'remove': true, 'popups': true };
   chrome.storage.sync.get(['tbf_init'], function(result) {
